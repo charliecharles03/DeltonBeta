51eb31f0f680eede5f2519764384a70d41908408
Chenchu Anil Kumar <37826280+charliecharles03@users.noreply.github.com>
2024-02-29 04:11:17 +0530

    1 added postgres db to the line so that the data is persistent (#5)
    * brother adding in dependencies that may be used in future not sure though :)
    
    * working finally understood traits atleast hope so thats the case

---
+++ src/controller.rs
@@ -0,0 +1,43 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
+
+
+#[get("/health_check")]
+pub async fn health_check() -> impl Responder {
+    let response = "bro its okay".to_string();
+    HttpResponse::Ok().json(response)
+}
+
+
+#[get("/brag")]
+pub async fn get_Brag() -> impl Responder {
+    let response = Brag {
+        title: "I am a title".to_string(),
+        description: "I am a description".to_string(),
+        impact: "I am an impact".to_string(),
+        difficulty: "I am a difficulty".to_string(),
+        date: "I am a date".to_string(),
+    }; 
+    HttpResponse::Ok().json(response.get_all_brags())
+}
+
+pub struct Brag {
+    pub title: String,
+    pub description: String,
+    pub impact : String,
+    pub difficulty: String,
+    pub date: String,
+}
+
+impl BragTrait for Brag {
+    fn get_all_brags(&self) -> String {
+        format!("{} {} {} {} {}", self.title, self.description, self.impact, self.difficulty, self.date)
+    }
+}
+
+pub trait BragTrait {
+    fn get_all_brags(&self) -> String;
+}
+
+
+
+
